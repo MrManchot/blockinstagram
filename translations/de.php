237f5d3dd38a774ebe6663e7c913eeeb8655e1ca
Cédric Mouleyre <mouleyrecedric@gmail.com>
2016-08-28 14:20:30 +0200

    [+] Add DE translation (Thank to @FriendsyDesign)

---
+++ translations/de.php
@@ -0,0 +1,15 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_c85f95c18f955e6cb9cfc6cb099f1fd7'] = 'Block Instagram';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_6ba576ebb868958014a02224f02fe852'] = 'Bilder von einem Instagram-Account anzeigen';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_c888438d14855d7d96a2724ee9c306bd'] = 'Einstellungen aktualisiert';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_4cd587a1fb63a482d93a6ff0fed1d940'] = 'Instagram-Benutzername:';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_46dbab4626fb6f4e773fbb527da28fde'] = 'Bildanzahl:';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_f32b763205b2ee5efcf8e45a7e3349a9'] = 'Bildgröße in Pixel:';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_8aa6b19a683137d22f7eaeecbca4fb65'] = 'Aktualisieren:';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_98f5449f725c2be645dc1be794d3b6df'] = 'Täglich';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_f826cc3c38088c39b0a05cf87435e08f'] = 'Stündlich';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_5708fc4fc126fb560f3da1782a17970d'] = 'Folge uns bei';
