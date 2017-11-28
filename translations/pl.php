40dbdf6090312eb3fdab54b1059bf745b36dab96
Cédric Mouleyre <mouleyrecedric@gmail.com>
2017-11-28 13:01:37 +0100

    Add Polish translation

---
+++ translations/pl.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_3903aab323863bd2e9b68218a7a65ebd'] = 'Śledz';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_ed2b5c0139cec8ad2873829dc1117d50'] = 'na';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_cc71d8f3dc8842c0f4f55e3f5b388ec6'] = 'polubień';
+$_MODULE['<{blockinstagram}prestashop>blockinstagram_a5d491060952aa8ad5fdee071be752de'] = 'komentarzy';
